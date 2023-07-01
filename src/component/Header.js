7b3ab5a9e771c9598228193e082857afc84e1d6e
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-07-01 16:42:57 +0530

    1-7-car-start

---
+++ src/component/Header.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div>
+            {/* Spinner Start */}
+            <div id="spinner" className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+                <div className="spinner-border text-primary" style={{ width: '3rem', height: '3rem' }} role="status">
+                    <span className="sr-only">Loading...</span>
+                </div>
+            </div>
+            {/* Spinner End */}
+            {/* Topbar Start */}
+            <div className="container-fluid bg-light p-0">
+                <div className="row gx-0 d-none d-lg-flex">
+                    <div className="col-lg-7 px-5 text-start">
+                        <div className="h-100 d-inline-flex align-items-center py-3 me-4">
+                            <small className="fa fa-map-marker-alt text-primary me-2" />
+                            <small>123 Street, New York, USA</small>
+                        </div>
+                        <div className="h-100 d-inline-flex align-items-center py-3">
+                            <small className="far fa-clock text-primary me-2" />
+                            <small>Mon - Fri : 09.00 AM - 09.00 PM</small>
+                        </div>
+                    </div>
+                    <div className="col-lg-5 px-5 text-end">
+                        <div className="h-100 d-inline-flex align-items-center py-3 me-4">
+                            <small className="fa fa-phone-alt text-primary me-2" />
+                            <small>+012 345 6789</small>
+                        </div>
+                        <div className="h-100 d-inline-flex align-items-center">
+                            <a className="btn btn-sm-square bg-white text-primary me-1" href><i className="fab fa-facebook-f" /></a>
+                            <a className="btn btn-sm-square bg-white text-primary me-1" href><i className="fab fa-twitter" /></a>
+                            <a className="btn btn-sm-square bg-white text-primary me-1" href><i className="fab fa-linkedin-in" /></a>
+                            <a className="btn btn-sm-square bg-white text-primary me-0" href><i className="fab fa-instagram" /></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* Topbar End */}
+            {/* Navbar Start */}
+            <nav className="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+                <a href="index.html" className="navbar-brand d-flex align-items-center px-4 px-lg-5">
+                    <h2 className="m-0 text-primary"><i className="fa fa-car me-3" />CarServ</h2>
+                </a>
+                <button type="button" className="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <div className="navbar-nav ms-auto p-4 p-lg-0">
+                        <a href="index.html" className="nav-item nav-link active">Home</a>
+                        <a href="about.html" className="nav-item nav-link">About</a>
+                        <a href="service.html" className="nav-item nav-link">Services</a>
+                        <div className="nav-item dropdown">
+                            <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <div className="dropdown-menu fade-up m-0">
+                                <a href="booking.html" className="dropdown-item">Booking</a>
+                                <a href="team.html" className="dropdown-item">Technicians</a>
+                                <a href="testimonial.html" className="dropdown-item">Testimonial</a>
+                                <a href="404.html" className="dropdown-item">404 Page</a>
+                            </div>
+                        </div>
+                        <a href="contact.html" className="nav-item nav-link">Contact</a>
+                    </div>
+                    <a href className="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Get A Quote<i className="fa fa-arrow-right ms-3" /></a>
+                </div>
+            </nav>
+            {/* Navbar End */}
+        </div>
+
+    );
+}
+
+export default Header;
